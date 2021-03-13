4c25cb8eeacf93c9952fff1a0170752609e66051
Fatemeh <fatemeh.marzoughi18@gmail.com>
2021-03-13 11:33:21 +0330

    BubbleSortFunction

--- app/main.cpp
+++ app/main.cpp
@@ -1,12 +1,44 @@
 
 #include <iostream>
 #include <stdlib.h>
+using namespace std;
 
-#include "Foo.h"
+// #include "Foo.h"
+
+void BubbleSort(int data[] , int n)
+{
+  int subArrayEnd = n-1;
+  int nextEnd = n-1;
+  do
+  {
+    for(int i = 0; i<subArrayEnd ; i++ )
+    {
+      if(data[i] > data[i+1])
+         swap(data[i] ,data[i+1]);
+      nextEnd = i;
+    }
+    subArrayEnd = nextEnd;
+  }
+  while(subArrayEnd > 0);
+  std::cout<<"Sorted List : ";
+  for(int i=0 ; i<n ; i++)
+    std::cout<<data[i]<<" , ";
+}
+
 int main()
 {
-  Foo d = Foo();
-  std::cout<< d.doSomething()<<std::endl;
-
+  // Foo d = Foo();
+  // std::cout<< d.doSomething()<<std::endl;
+  int input[10];
+  for(int i=0 ; i<10 ; i++)
+  {
+    std::cout<<"["<<i<<"] = ";
+    std::cin>>input[i];
+  }
+  std::cout<<"Unsorted List : ";
+  for(int i=0 ; i<10 ; i++)
+    std::cout<<input[i]<<" , ";
+  std::cout<<"Sorting..."<<std::endl;
+  BubbleSort(input , 10);
   return 0;
 }
